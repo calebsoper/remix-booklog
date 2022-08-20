fa2c73f883f9ed6ee8a6f1660136264debf85df8
Caleb Soper <csoper@add123.com>
2022-08-20 13:10:29 -0400

    added files

---
+++ public/build/routes/healthcheck-C2QRCUOP.js
@@ -0,0 +1,14 @@
+import {
+  __commonJS,
+  init_react
+} from "/build/_shared/chunk-NTN7YPXJ.js";
+
+// browser-route-module:/home/csoper/git/indie-stack/app/routes/healthcheck.tsx?browser
+var require_healthcheck = __commonJS({
+  "browser-route-module:/home/csoper/git/indie-stack/app/routes/healthcheck.tsx?browser"(exports, module) {
+    init_react();
+    module.exports = {};
+  }
+});
+export default require_healthcheck();
+//# sourceMappingURL=/build/routes/healthcheck-C2QRCUOP.js.map
---
+++ public/build/routes/logout-TSIHZS3M.js
@@ -0,0 +1,14 @@
+import {
+  __commonJS,
+  init_react
+} from "/build/_shared/chunk-NTN7YPXJ.js";
+
+// browser-route-module:/home/csoper/git/indie-stack/app/routes/logout.tsx?browser
+var require_logout = __commonJS({
+  "browser-route-module:/home/csoper/git/indie-stack/app/routes/logout.tsx?browser"(exports, module) {
+    init_react();
+    module.exports = {};
+  }
+});
+export default require_logout();
+//# sourceMappingURL=/build/routes/logout-TSIHZS3M.js.map
